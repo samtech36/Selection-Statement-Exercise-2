c6184d7e6d781c95efcfa9d7bce4a4e1175d89ba
samtech36 <samuel.hossain@outlook.com>
2024-08-13 14:43:47 -0400

    My Favorite School Subject Program

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavoriteSchoolSubject();
+        }
+
+        public static void FavoriteSchoolSubject()
+        {
+            Console.WriteLine("What is your favorite subject? ");
+            string subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "math":
+                    Console.WriteLine("I love math and numbers.");
+                    break;
+                case "biology":
+                    Console.WriteLine("I love biology because I like to learn about plants and animals.");
+                    break;
+                case "literature":
+                    Console.WriteLine("I hate literature because reading complicated words is hectic.");
+                    break;
+                case "history":
+                    Console.WriteLine("I love history because I like to learn about U.S. history and how it all started. ");
+                    break;
+                case "writing":
+                    Console.WriteLine("I hate writing because writing essays takes so much time.");
+                    break;
+                default:
+                    Console.WriteLine("I do not know about that subject");
+                    break;
+            }
+
         }
     }
 }
